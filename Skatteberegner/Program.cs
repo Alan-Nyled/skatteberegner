b6b20929451694082f6cc4379c1b47df79918054
alannyled <84908245+alannyled@users.noreply.github.com>
2023-09-27 12:47:37 +0200

    nye beregninger

--- Skatteberegner/Program.cs
+++ Skatteberegner/Program.cs
@@ -6,6 +6,12 @@
         // Funktionen returnerer det beløb, der skal beskattes (altså ikke selve skatten).
         public double SkatVedJulegave(double julegave, double andenGave) {
             double gavesum = julegave + andenGave;
+            if(gavesum <= 1200){
+                return 0;
+            }
+            if(andenGave > 300 && julegave < 900){
+                return andenGave;
+            }
             return gavesum;
         }
     }
